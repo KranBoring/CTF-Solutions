727210654d768ff1c6fb0e5a2081f761d87a8a5c
KranBoring <oopsnaiga2008@gmail.com>
2026-09-23 09:20:57 +0000

    Cap Nhap

--- WannaGame Event/Crypto/Cua_da_tro_lai/test.py
+++
@@ -1,4 +0,0 @@
-from Crypto.Util.number import getPrime
-#
-for i in range(10):
-    print(getPrime(256))
--- WannaGame Event/Web/hachimi/app/src/routes/home.tsx
+++
@@ -1,7 +0,0 @@
-import { Elysia } from "elysia";
-import { Html } from "@elysiajs/html";
-import { Home } from "../ui/home";
-
-export function homeRoute(app: Elysia) {
-  return app.get("/", () => <Home />);
-}
