7c4106138c030044126f41aa62315b97b29840f3
KranBoring <oopsnaiga2008@gmail.com>
2026-09-23 09:17:42 +0000

    Cap Nhap

--- PicoCTF/Easy1/decrypt.py
+++
@@ -1,8 +0,0 @@
-key = "SOLVECRYPTO"
-flag_enc = "UFJKXQZQUNB"
-
-offset = ord('A')
-dec = ""
-for i in range(len(flag_enc)):
-    dec += chr((ord(flag_enc[i]) - ord(key[i])) % 26 + offset)
-print(dec)
